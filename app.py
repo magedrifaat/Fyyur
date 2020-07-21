40441d7965ebd5fe6275d47ac624a8e6c96c0cd9
maged <magedrifaat@gmail.com>
2020-07-22 01:08:31 +0200

    Implemented repr frunction for the models for easier inspection

--- app.py
+++ app.py
@@ -48,6 +48,10 @@ class Venue(db.Model):
 
     shows = db.relationship('Show', backref='venue', cascade="all, delete-orphan")
 
+    def __repr__(self):
+        return f'<Venue id={self.id} name={self.name}>'
+
+
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
@@ -66,8 +70,12 @@ class Artist(db.Model):
     seeking_description = db.Column(db.String())
 
     shows = db.relationship('Show', backref='artist', cascade="all, delete-orphan")
+  
+    def __repr__(self):
+        return f'<Artist id={self.id} name={self.name}>'
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+# TODO DONE Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 # Implementing Show model as an association object since it is basically an association table with an extra field
 # This is per SQLAlchey documentation here: https://docs.sqlalchemy.org/en/13/orm/basic_relationships.html#association-object
 class Show(db.Model):
@@ -77,6 +85,9 @@ class Show(db.Model):
   artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
   venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
   show_time = db.Column(db.DateTime(), primary_key=True)
+
+  def __repr__(self):
+      return f'<Show artist_id={self.artist_id} venue_id={self.venue_id} time={self.show_time}>'
 
 #----------------------------------------------------------------------------#
 # Filters.
